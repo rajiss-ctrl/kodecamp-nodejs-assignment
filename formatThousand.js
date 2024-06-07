8dc101dce3a771a8b35ebb265be949d71a41288f
rajiss-ctrl <omosanjos77#gmail.com>
2024-06-07 21:10:19 +0100

    assignment branch

---
+++ formatThousand.js
@@ -0,0 +1,7 @@
+function formatThousand(number) {
+    return number.toLocaleString();
+}
+
+// Example usage
+console.log(formatThousand(1000000)); // Output: 1,000,000
+console.log(formatThousand(10000000)); // Output: 10,000,000
